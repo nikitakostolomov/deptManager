ca26f432364954bba2ffa1dd76a341aea18edf7b
NikitaKostolomov <didrexov@gmail.com>
2020-12-19 22:12:25 +0300

    skeleton of the project

---
+++ deptManager/src/test/java/com/deptManager/deptManager/DeptManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deptManager.deptManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeptManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
